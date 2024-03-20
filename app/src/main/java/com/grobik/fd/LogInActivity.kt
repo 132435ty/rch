2d109733740e488ff665e8fc282a2ce2e7c44269
132435ty <Kz89267908477@gmail.com>
2024-03-20 22:30:37 +0300

    1233

--- app/src/main/java/com/grobik/fd/LogInActivity.kt
+++ app/src/main/java/com/grobik/fd/LogInActivity.kt
@@ -1,20 +1,24 @@
 package com.grobik.fd
 
+import android.graphics.Paint
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class LogInActivity : AppCompatActivity() {
+    var TIMEOUT_MILLIS = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.log_in)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val text = findViewById<TextView>(R.id.password10)
+        text.paintFlags = Paint.UNDERLINE_TEXT_FLAG
+        text.setOnClickListener {
+            if (text.text != "Что-то") {
+                text.text = "Что-то"
+            } else {
+                text.text = "Здравствуйте"
+            }
+
         }
     }
 }
